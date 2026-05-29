93d8a5023f2a9bc425d4cc90dd7db95cb0f67704
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-05-29 20:07:47 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import "tailwindcss";
+import Navbar from './Navbar.jsx';
+import Footer from './Footer.jsx';
+import Content from './Content.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Content/>
+      <Footer/>
+    </div>
+    
+  )
+}
+
+export default App;
